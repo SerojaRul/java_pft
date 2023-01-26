7619c4598724447a4829d55dde125252fe9bf746
Seroja Rul <53050319+SerojaRul@users.noreply.github.com>
2023-01-26 11:53:23 +0300

    Update MyFirstProgram.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,6 +4,10 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+
+    System.out.println(2 + 2);
+
+    System.out.println("2" + "2");
   }
 
 }
